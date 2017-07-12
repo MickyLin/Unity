3bc49c7926ad2c00d1b2b5a17d52884a94000f79
MickyLin <mickylin0117@gmail.com>
2017-07-12 14:17:16 +0800

    First Commit
    Add Unity Project to Github

---
+++ AR_House/Assets/_Script/UI_manager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_manager : MonoBehaviour {
+
+
+    public GameObject Inside_Panel;//放Inside_Panel
+    public GameObject Building;//放建築物
+    public GameObject Plan1;
+    public GameObject Plan2;
+    public GameObject Plan3;
+
+    public GameObject Example_Panel;
+    public Text Example_Content_Text;
+    public Image Example_Content_Image;
+    public Sprite[] SpriteChange;
+    public string[] NameChange;
+
+
+
+    //外部效果
+    public void Outside_Button()
+    {
+        Building.SetActive(!Building.activeSelf);
+        Inside_Panel.SetActive(false);
+        Plan1.SetActive(false);
+        Plan3.SetActive(false);
+        Plan2.SetActive(false);
+    }
+
+    //內部構造
+    public void Inside_Button()
+    {
+        Inside_Panel.SetActive(!Inside_Panel.activeSelf);
+        Building.SetActive(false);
+    }
+
+    public void Plan1_Button()//房間一
+    {
+        Inside_Panel.SetActive(false);//把內部構造面板關掉
+        Building.SetActive(false);
+        Plan1.SetActive(true);
+        Plan3.SetActive(false);
+        Plan2.SetActive(false);
+    }
+
+    public void Plan2_Button()//房間二
+    {
+        Inside_Panel.SetActive(false);//把內部構造面板關掉
+        Plan2.SetActive(true);
+        Plan1.SetActive(false);
+        Plan3.SetActive(false);
+    }
+
+    public void Plan3_Button()//房間三
+    {
+        Inside_Panel.SetActive(false);//把內部構造面板關掉
+        Plan3.SetActive(true);
+        Plan2.SetActive(false);
+        Plan1.SetActive(false);
+    }
+
+}
+
+
+
+
+
