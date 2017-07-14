85d03ede67468fd724b481ace0aa457286426ccd
MickyLin <mickylin0117@gmail.com>
2017-07-14 10:28:46 +0800

    Update new Function
    Update Example and code.

--- AR_House/Assets/_Script/UI_manager.cs
+++ AR_House/Assets/_Script/UI_manager.cs
@@ -11,11 +11,11 @@ public class UI_manager : MonoBehaviour {
     public GameObject Plan2;
     public GameObject Plan3;
 
-    public GameObject Example_Panel;
-    public Text Example_Content_Text;
+    public GameObject Example_Panel;//面板
+    public Text Example_Content_Text;//建物名字
     public Image Example_Content_Image;
-    public Sprite[] SpriteChange;
-    public string[] NameChange;
+    public Sprite[] SpriteChange;//圖片
+    public string[] NameChange;//建築物名字
 
 
 
@@ -27,6 +27,7 @@ public class UI_manager : MonoBehaviour {
         Plan1.SetActive(false);
         Plan3.SetActive(false);
         Plan2.SetActive(false);
+        Example_Panel.SetActive(false);
     }
 
     //內部構造
@@ -34,15 +35,19 @@ public class UI_manager : MonoBehaviour {
     {
         Inside_Panel.SetActive(!Inside_Panel.activeSelf);
         Building.SetActive(false);
+        Example_Panel.SetActive(false);
+
     }
 
     public void Plan1_Button()//房間一
     {
+       
         Inside_Panel.SetActive(false);//把內部構造面板關掉
         Building.SetActive(false);
         Plan1.SetActive(true);
         Plan3.SetActive(false);
         Plan2.SetActive(false);
+        
     }
 
     public void Plan2_Button()//房間二
@@ -51,6 +56,7 @@ public class UI_manager : MonoBehaviour {
         Plan2.SetActive(true);
         Plan1.SetActive(false);
         Plan3.SetActive(false);
+       
     }
 
     public void Plan3_Button()//房間三
@@ -59,8 +65,22 @@ public class UI_manager : MonoBehaviour {
         Plan3.SetActive(true);
         Plan2.SetActive(false);
         Plan1.SetActive(false);
+        
     }
 
+    public void Example_Button()//應用案例
+    {
+        Example_Panel.SetActive(!Example_Panel.activeSelf);
+        Building.SetActive(false);
+        Inside_Panel.SetActive(false);
+        Plan2.SetActive(false);
+        Plan1.SetActive(false);
+        Plan3.SetActive(false);
+    }
+    public void Example_Close_Button()
+    {
+        Example_Panel.SetActive(false);
+    }
 }
 
 
